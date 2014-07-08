347a7bd9a01c58efa52137b8e79c152f6af16ada
abdollah110 <smohamadi@gmail.com>
2014-07-08 14:50:43 +0200

    SYNC

--- Batch_Submit_v3.py
+++ Batch_Submit_v3.py
@@ -19,85 +19,13 @@ Run_Over = {
     4:("/pnfs/iihe/cms/store/user/abdollah/HTTNtuples/53X/Embedded", "embed12", "45:00:00"),
     
 }
-
     ########################################################################################
-#sample_info = samples.split()
-#command1 = "[" + sample_info[0].replace("/","",1).partition("/")[0] + "]" + "\n"
-
-#def make_submit_form(order, pnfn, data_year, timing):
-
-def make_submit_form():
-    TextSamples = open("SelectSample.txt", "r")
-    location = os.getcwd()
-#    location = location.replace("/localgrid_mnt", "") #nedd to remove the first part of address while submitting
-#    Sample = os.popen(("ls " + pnfn + " | sort "))
-#    name_submitFile = "Submit_" + data_year + ".sh"
-#    name_haddFile = "Hadd_" + data_year  + ".sh"
-#    submit_File = open(name_submitFile, 'w')
-#    Hadd_File = open(name_haddFile, 'w')
-    #Make loop over the files in the given directories
-    name_submitFile = "Submit_" +  "_"  + ".sh"
-    name_haddFile = "Hadd_" +  ".sh"
-    submit_File = open(name_submitFile, 'w')
-    Hadd_File = open(name_haddFile, 'w')
-    for files in TextSamples.readlines():
-#        print files[0:-1]
-#        for i in files[0:-1].split(","):
-        pnfn= files[0:-1].split(",")[0]
-        data_year= files[0:-1].split(",")[1]
-        timing= files[0:-1].split(",")[2]
-#        print pnfn, data_year, timing
-        sampleName2= files[0:-1].split("/")[10]
-#        print "sampleName2=", sampleName2
-        sampleName= sampleName2.split(",")[0]
-        print "sampleName=", sampleName
-        
-
-        for numMod in xrange(0,10):
-            f = os.popen("ls " + pnfn + "/" + " | sort ")
-            dir = "dcap://maite.iihe.ac.be" + pnfn + "/" + sampleName+ "/"
-            name_out = "_" + data_year + "_" +  sampleName +"_"+str(numMod)+ ".sh"
-            outFile = open(name_out, 'w')
-            command1 = "source $VO_CMS_SW_DIR/cmsset_default.sh " + "\n"
-            command1 = command1 + "cd " + location + "\n"
-            command1 = command1 + "eval `scram runtime -sh` " + "\n\n"
-            command1 = command1 + "mkdir    Out_" + sampleName +"_"+str(numMod)+ "\n"
-            outFile.write(command1)
-            #Make loop over the rootfiles in the given file
-            for i in f.readlines():
-                QName=i[0:-1]
-                XName=int(float(QName[15:-11]))
-                outName= sampleName + "_"+str(XName) +".root"
-                if (XName % 10 == numMod):
-                    command2 = "\n" + "./nMSSM_Analysis.exe " + data_year + " "   +outName + " " + dir + "/" + i[0:-1]
-                    command2 = command2 + " \n" + " mv  " + data_year + "_" +  outName + "\t" + "Out_" + files[0:-1]+"_"+str(numMod)
-                    command2 = command2 + " \n\n\n"
-                    outFile.write(command2)
-
-            #Writing on out Files
-#            command3 = "qsub -q localgrid@cream02.wn -o " + files[0:-1] + ".stdout -e " + files[0:-1] + ".stderr -l walltime=" + timing + "  " + name_out + "\n"
-            Name1=sampleName
-            shortName=Name1[0:-10]+"_"+str(numMod)
-            command3 = "qsub -q localgrid@cream02 -o " + shortName + ".stdout -e " + shortName + ".stderr -l walltime=" + timing + "  " + name_out + "\n"
-            command4 = "hadd -f ROOT/" + data_year + "/" + sampleName +"_"+str(numMod)+ ".root\t" + "Out_" + sampleName +"_"+str(numMod)+ "/*.root" + "\n"
-            submit_File.write(command3)
-            Hadd_File.write(command4)
-
-    outFile.close()
-    submit_File.close()
-    Hadd_File.close()
 def make_Ready(order, pnfn, data_year, timing,outFile):
 	
 	Sample = os.popen(("ls " + pnfn + " | sort "))
 	for files in Sample.readlines():
-		command= ""
-#		command = command +  '"'+ pnfn + "/" +files[0:-1] + '","'+ data_year+ '","'  + timing+'"\n'
-		command = command +  ''+ pnfn + "/" +files[0:-1] + ','+ data_year+ ','  + timing+'\n'
+		command =   pnfn + "/" +files[0:-1] + ','+ data_year+ ','  + timing+'\n'
 		outFile.write(command)
-	
-
-		
-
 
     ########################################################################################
 if __name__ == "__main__":
@@ -108,9 +36,5 @@ if __name__ == "__main__":
 		
 	make_Ready(str(i), R1, R2, R3, outFile)
      outFile.close()
-     
-     make_submit_form()
 
-#    rm TotalSubmit.sh; ls Submit_* | xargs -n 1 -I {} echo 'sh {}' >> TotalSubmit.sh
-#    rm TotalHadd.sh;   ls Hadd_* | xargs -n 1 -I {} echo 'sh {}' >> TotalHadd.sh
     
