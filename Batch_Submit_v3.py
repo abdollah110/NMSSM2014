90799b9fab58086300ba82c4c8a45c3ce6bf7e82
abdollah110 <smohamadi@gmail.com>
2014-07-08 13:49:21 +0200

    SYNC

--- Batch_Submit_v3.py
+++ Batch_Submit_v3.py
@@ -38,48 +38,51 @@ def make_submit_form():
     #Make loop over the files in the given directories
     for files in TextSamples.readlines():
         print files[0:-1]
-        for i in files[0:-1].split(","):
-#            sample_info = files.split()
-            print i
-#        for numMod in xrange(0,10):
-#            f = os.popen("ls " + TextSamples + "/" + files[0:-1] + " | sort ")
-#            dir = "dcap://maite.iihe.ac.be" + TextSamples + "/" + files[0:-1] + "/"
-#            name_out = "_" + data_year + "_" +  files[0:-1] +"_"+str(numMod)+ ".sh"
-#            outFile = open(name_out, 'w')
-#            command1 = "source $VO_CMS_SW_DIR/cmsset_default.sh " + "\n"
-#            command1 = command1 + "cd " + location + "\n"
-#            command1 = command1 + "eval `scram runtime -sh` " + "\n\n"
-#            command1 = command1 + "mkdir    Out_" + files[0:-1] +"_"+str(numMod)+ "\n"
-#            outFile.write(command1)
-#            #Make loop over the rootfiles in the given file
-#            for i in f.readlines():
-#                QName=i[0:-1]
-#                XName=int(float(QName[15:-11]))
-#                outName= files[0:-1] + "_"+str(XName) +".root"
-#                if (XName % 10 == numMod):
-#                    command2 = "\n" + "./nMSSM_Analysis.exe " + data_year + " "   +outName + " " + dir + "/" + i[0:-1]
-#                    command2 = command2 + " \n" + " mv  " + data_year + "_" +  outName + "\t" + "Out_" + files[0:-1]+"_"+str(numMod)
-#                    command2 = command2 + " \n\n\n"
-#                    outFile.write(command2)
-#
-#            #Writing on out Files
-##            command3 = "qsub -q localgrid@cream02.wn -o " + files[0:-1] + ".stdout -e " + files[0:-1] + ".stderr -l walltime=" + timing + "  " + name_out + "\n"
-#            Name1=files[0:-1]
-#            shortName=Name1[0:-10]+"_"+str(numMod)
-#            command3 = "qsub -q localgrid@cream02 -o " + shortName + ".stdout -e " + shortName + ".stderr -l walltime=" + timing + "  " + name_out + "\n"
-#            command4 = "hadd -f ROOT/" + data_year + "/" + files[0:-1] +"_"+str(numMod)+ ".root\t" + "Out_" + files[0:-1] +"_"+str(numMod)+ "/*.root" + "\n"
-#            submit_File.write(command3)
-#            Hadd_File.write(command4)
-#
-#    outFile.close()
-#    submit_File.close()
-#    Hadd_File.close()
+#        for i in files[0:-1].split(","):
+        pnfn= files[0:-1].split(",")[0]
+        data_year= files[0:-1].split(",")[1]
+        timing= files[0:-1].split(",")[2]
+
+        for numMod in xrange(0,10):
+            f = os.popen("ls " + pnfn + "/" + files[0:-1] + " | sort ")
+            dir = "dcap://maite.iihe.ac.be" + pnfn + "/" + files[0:-1] + "/"
+            name_out = "_" + data_year + "_" +  files[0:-1] +"_"+str(numMod)+ ".sh"
+            outFile = open(name_out, 'w')
+            command1 = "source $VO_CMS_SW_DIR/cmsset_default.sh " + "\n"
+            command1 = command1 + "cd " + location + "\n"
+            command1 = command1 + "eval `scram runtime -sh` " + "\n\n"
+            command1 = command1 + "mkdir    Out_" + files[0:-1] +"_"+str(numMod)+ "\n"
+            outFile.write(command1)
+            #Make loop over the rootfiles in the given file
+            for i in f.readlines():
+                QName=i[0:-1]
+                XName=int(float(QName[15:-11]))
+                outName= files[0:-1] + "_"+str(XName) +".root"
+                if (XName % 10 == numMod):
+                    command2 = "\n" + "./nMSSM_Analysis.exe " + data_year + " "   +outName + " " + dir + "/" + i[0:-1]
+                    command2 = command2 + " \n" + " mv  " + data_year + "_" +  outName + "\t" + "Out_" + files[0:-1]+"_"+str(numMod)
+                    command2 = command2 + " \n\n\n"
+                    outFile.write(command2)
+
+            #Writing on out Files
+#            command3 = "qsub -q localgrid@cream02.wn -o " + files[0:-1] + ".stdout -e " + files[0:-1] + ".stderr -l walltime=" + timing + "  " + name_out + "\n"
+            Name1=files[0:-1]
+            shortName=Name1[0:-10]+"_"+str(numMod)
+            command3 = "qsub -q localgrid@cream02 -o " + shortName + ".stdout -e " + shortName + ".stderr -l walltime=" + timing + "  " + name_out + "\n"
+            command4 = "hadd -f ROOT/" + data_year + "/" + files[0:-1] +"_"+str(numMod)+ ".root\t" + "Out_" + files[0:-1] +"_"+str(numMod)+ "/*.root" + "\n"
+            submit_File.write(command3)
+            Hadd_File.write(command4)
+
+    outFile.close()
+    submit_File.close()
+    Hadd_File.close()
 def make_Ready(order, pnfn, data_year, timing,outFile):
 	
 	Sample = os.popen(("ls " + pnfn + " | sort "))
 	for files in Sample.readlines():
 		command= ""
-		command = command +  '"'+ pnfn + "/" +files[0:-1] + '","'+ data_year+ '","'  + timing+'"\n'
+#		command = command +  '"'+ pnfn + "/" +files[0:-1] + '","'+ data_year+ '","'  + timing+'"\n'
+		command = command +  ''+ pnfn + "/" +files[0:-1] + ' , '+ data_year+ ' , '  + timing+'\n'
 		outFile.write(command)
 	
 
