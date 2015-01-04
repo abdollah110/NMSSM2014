59fa909896992bd207c082bfc08787c9420cbc06
abdollah110 <smohamadi@gmail.com>
2015-01-04 23:27:50 +0100

    SYNC

--- Batch_Submit_v3.py
+++ Batch_Submit_v3.py
@@ -15,11 +15,9 @@ Run_Over = {
     ##### Unfiltered
     1:("/pnfs/iihe/cms/store/user/abdollah/HTTNtuples_v5/53X/MC", "mc12", "75:00:00"),
     2:("/pnfs/iihe/cms/store/user/abdollah/HTTNtuples_v5/53X/Data", "data12", "75:00:00"),
-    3:("/pnfs/iihe/cms/store/user/abdollah/HTTNtuples_v4/Embed_MC", "embedmc12", "75:00:00"),
-    4:("/pnfs/iihe/cms/store/user/abdollah/HTTNtuples_v4/Embed_Data", "embeddata12", "75:00:00"),
-
     5:("/pnfs/iihe/cms/store/user/ccaillol/HTTNtuples_v4/53X/MC", "mc12", "75:00:00"),
-
+    6:("/pnfs/iihe/cms/store/user/abdollah/HTTNtuples_v4/Embed_MC", "embedmc12", "75:00:00"),
+    7:("/pnfs/iihe/cms/store/user/abdollah/HTTNtuples_v4/Embed_Data", "embeddata12", "75:00:00"),
 
 }
     ########################################################################################
